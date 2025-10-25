74ef9161f61a3c05533090dca03ef6a5f77e4c43
kat <kmartinezmedia@gmail.com>
2025-10-25 16:51:01 -0400

    test server action with bun in client comp

--- actions/transform-code.tsx
+++ actions/transform-code.tsx
@@ -2,12 +2,7 @@
 
 import { $ } from 'bun';
 
-const cssInput = `
-  @import "tailwindcss";
-  @custom-variant dark (&:where(.dark, .dark *));
-`;
-
-export async function transformCode() {
-  const result = await $`echo ${cssInput} | tailwindcss -i -`.text();
+export async function transformCode(tailwindCss: string) {
+  const result = await $`echo ${tailwindCss} | tailwindcss -i -`.text();
   return result;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,45 @@
-'use server';
+'use client';
 
+import { useRef, useState, useTransition } from 'react';
 import { transformCode } from '@/actions/transform-code';
 
-export default async function Home() {
-  const result = await transformCode();
+const initialCss = `
+  @import "tailwindcss";
+  @custom-variant dark (&:where(.dark, .dark *));
+`;
 
-  return <div>{result}</div>;
+export default function Home() {
+  const [tailwindCss, setTailwindCss] = useState(initialCss);
+  const [cssOutput, setCssOutput] = useState('');
+  const [isPending, startTransition] = useTransition();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const addClassName = () => {
+    const classNameToAdd = inputRef.current?.value;
+    if (!classNameToAdd) return;
+    startTransition(async () => {
+      const combinedCss = `${tailwindCss} @source inline("${classNameToAdd}");`;
+      const result = await transformCode(combinedCss);
+      setTailwindCss(combinedCss);
+      setCssOutput(result);
+    });
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Enter className"
+        disabled={isPending}
+        type="text"
+        ref={inputRef}
+      />
+      <button type="button" onClick={addClassName} disabled={isPending}>
+        Add Class
+      </button>
+
+      <pre style={{ textWrap: 'pretty' }}>
+        <code>{cssOutput}</code>
+      </pre>
+    </div>
+  );
 }
