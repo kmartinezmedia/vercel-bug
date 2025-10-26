12e0b5ecb3c0856220f35879f28081e1267ea130
kat <kmartinezmedia@gmail.com>
2025-10-25 22:37:40 -0400

    try non bun api

--- actions/transform-code.tsx
+++ actions/transform-code.tsx
@@ -1,8 +1,28 @@
 'use server';
 
-import { $ } from 'bun';
+import { spawn } from 'node:child_process';
+import { once } from 'node:events';
 
 export async function transformCode(cssInput: string) {
-  const result = await $`echo ${cssInput} | tailwindcss -i -`.text();
-  return result;
+  const tailwind = spawn('tailwindcss', ['-i', '-'], {
+    stdio: ['pipe', 'pipe', 'pipe'],
+  });
+
+  let stdout = '';
+  let stderr = '';
+
+  tailwind.stdout.on('data', (d) => {
+    stdout += d;
+  });
+
+  tailwind.stderr.on('data', (d) => {
+    stderr += d;
+  });
+
+  tailwind.stdin.end(cssInput);
+
+  const [code] = await once(tailwind, 'close');
+
+  if (code === 0) return stdout;
+  throw new Error(`Tailwind CSS exited with code ${code}: ${stderr}`);
 }
