8cc01c86ccc6f01bceef91bdb7f919d7617b2e98
kat <kmartinezmedia@gmail.com>
2025-10-25 23:02:58 -0400

    update outputFileTracingIncludles

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['tailwindcss', '@tailwindcss/postcss', 'postcss'],
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/.bin/tailwindcss', './node_modules/@tailwindcss/**'],
+  },
 };
 
 export default nextConfig;
