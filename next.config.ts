11e14cab97a37fc982f29338d66fda2d9920e857
kat <kmartinezmedia@gmail.com>
2025-10-26 00:13:57 -0400

    try without deps

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from 'next';
-import tailwindDeps from './tailwind-transitive-deps.json';
+// import tailwindDeps from './tailwind-transitive-deps.json';
 
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['tailwindcss', '@tailwindcss/postcss'],
-  outputFileTracingIncludes: {
-    '/*': tailwindDeps,
-  },
 };
 
 export default nextConfig;
