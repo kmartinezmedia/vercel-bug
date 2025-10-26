373a36b3d9da60a27d93f22716f101b00799d4df
kat <kmartinezmedia@gmail.com>
2025-10-25 22:18:09 -0400

    test in client comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
-'use server';
+'use client';
 
+import { useEffect, useRef, useState } from 'react';
 import { transformCode } from '@/actions/transform-code';
 
-export default async function Home() {
-  const result = await transformCode();
+const initialCss = `
+  @import "tailwindcss";
+  @custom-variant dark (&:where(.dark, .dark *));
+`;
 
-  return <div>{result}</div>;
+export default function Home() {
+  const classNamesToAddRef = useRef<string[]>([]);
+  const [result, setResult] = useState('');
+  useEffect(() => {
+    transformCode(initialCss).then(setResult);
+  }, []);
+
+  return (
+    <div>
+      <form
+        action={async (formData) => {
+          const classNameToAdd = formData.get('className') as string;
+          classNamesToAddRef.current.push(classNameToAdd);
+          const cssInput = `${initialCss}\n@source inline("${classNamesToAddRef.current.join(' ')}");`;
+          transformCode(cssInput).then(setResult);
+        }}
+      >
+        <input type="text" name="className" placeholder="Enter a class name" />
+      </form>
+      <div>
+        <pre style={{ whiteSpace: 'pre-wrap' }}>
+          <code>{result}</code>
+        </pre>
+      </div>
+    </div>
+  );
 }
