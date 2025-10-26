bd246e0af8ebf031a8113e15f81a73cdaffd0dc1
kat <kmartinezmedia@gmail.com>
2025-10-25 23:00:34 -0400

    save route change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { transformCode } from '@/actions/transform-code';
+import { transformCode } from '@/app/actions/transform-code';
 
 const initialCss = `
   @import "tailwindcss";
