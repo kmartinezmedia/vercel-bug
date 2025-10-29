729fcb68fe9c123dbabaed77e811b3f54ed828ee
Katherine Martinez <kmartinezmedia@gmail.com>
2025-10-29 06:18:44 -0700

    verify bun runtime works

--- actions/transform-code.tsx
+++ actions/transform-code.tsx
@@ -2,12 +2,7 @@
 
 import { $ } from 'bun';
 
-const cssInput = `
-  @import "tailwindcss";
-  @custom-variant dark (&:where(.dark, .dark *));
-`;
-
-export async function transformCode() {
+export async function transformCode(cssInput: string) {
   const result = await $`echo ${cssInput} | tailwindcss -i -`.text();
   return result;
 }
